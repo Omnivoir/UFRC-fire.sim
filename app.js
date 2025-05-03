14b072ff99b11aa1a72282c4dcaa9807cdb90fda
Omnivoir.software <stewmin20k@gmail.com>
2025-05-03 14:45:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const map = L.map('map').setView([36.8656, -87.4886], 13); // Centered Hopkinsville, KY
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data © OpenStreetMap contributors'
+}).addTo(map);
+
+// Placeholder for RHQC Overlay here later
